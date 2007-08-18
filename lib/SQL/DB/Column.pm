cb014d11455950c14c391b449546386ee04b307a
Mark Lawrence <nomad@null.net>
2007-08-18 17:51:52 +0200

    Turned SQL::DB::Column->references() into a lazy inflation function. This means we don't care about the order in which tables are defined, although it is still important because the database (eg PostgreSQL) still cares.

--- lib/SQL/DB/Column.pm
+++ lib/SQL/DB/Column.pm
@@ -6,16 +6,17 @@ use overload '""' => 'as_string', fallback => 1;
 use Carp qw(carp croak);
 use Scalar::Util qw(weaken);
 
+        use Data::Dumper;
+        $Data::Dumper::Indent = 1;
+
 SQL::DB::Column->mk_accessors(qw(
     table
     name
-    primary
     type
     null
     default
     unique
     auto_increment
-    references
 ));
 
 
@@ -29,36 +30,78 @@ sub table {
         unless(ref($table) and ref($table) eq 'SQL::DB::Table') {
             croak "table must be a SQL::DB::Table";
         }
-        $self->{'table'} = $table;
-        weaken($self->{'table'});
+        $self->{table} = $table;
+        weaken($self->{table});
     }
-    return $self->{'table'};
+    return $self->{table};
 }
 
 
+sub primary {
+    my $self = shift;
+    if (@_) {
+        if ($self->{primary} = shift) {
+            $self->{table}->add_primary($self);
+        }
+    }
+    else {
+        return $self->{primary};
+    }
+}
+
+
+#
+# This is a delayed value function. Takes a string, but first time
+# is accessed it finds the real column and sets itself to that column.
+#
 sub references {
     my $self = shift;
-    if ( @_ ) {
-        my $col = shift;
-        unless(ref($col) and ref($col) eq 'SQL::DB::Column') {
-            croak "reference must be a SQL::DB::Column";
-        }
-        $self->{'references'} = $col;
-        weaken($self->{'references'});
+    # Set a value
+    if (@_) {
+        $self->{references} = shift;
+        return;
     }
-    return $self->{'references'};
+
+    # Not set
+    if (!$self->{references}) {
+        return;
+    }
+
+    # Already accessed - return the reference to SQL::DB::Column
+    if (ref($self->{references})) {
+        return $self->{references};
+    }
+
+    # Not yet accessed - find the reference to SQL::DB::Column
+    my @cols = $self->table->text2cols($self->{references});
+    $self->{references} = $cols[0];
+    weaken($self->{references});
+#   $col->table->has_many($self);
+    return $self->{references};
 }
 
+
+sub default_sql {
+    my $self = shift;
+    my $default = $self->default;
+    if (!defined($default)) {
+        return '';
+    }
+
+    if ($self->type =~ m/(int)|(real)|(float)|(double)|(numeric)/i) {
+        return ' DEFAULT ' . $default
+    }
+    return " DEFAULT '" . $default ."'";
+}
 
 sub sql {
     my $self = shift;
     return sprintf('%-15s %-15s', $self->name, $self->type)
            . ($self->null ? 'NULL' : 'NOT NULL')
-           . (defined($self->default) ? ' DEFAULT '
-               . (defined($self->default) ? '?' : 'NULL') : '')
+           . $self->default_sql
            . ($self->auto_increment ? ' AUTO_INCREMENT' : '')
            . ($self->unique ? ' UNIQUE' : '')
-           . ($self->primary ? ' PRIMARY KEY' : '')
+#           . ($self->primary ? ' PRIMARY KEY' : '')
            . ($self->references ? ' REFERENCES '
                . $self->references->table->name .'('
                . $self->references->name .')' : '')
