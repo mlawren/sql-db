mlawren/sql-db

commit 49404cf9a931efde23842905bdea164a1b841fb4

Implement SQL::DBx::Sequence using ATTACH instead of separate dbh.
Less objects all round for better efficiency. May not be suitable for
multiple accesses to database at the same time, as I believe
transactions will rollback the seq.table changes? Probably doesn't
roll back the last_insert_rowid() result though ... need to check.

lib/SQL/DB.pm
@@ -199,6 +199,10 @@ around BUILDARGS => sub {
     return $class->$orig(%args);
 };
 
+# For our extensions to 'around' or 'after'
+sub BUILD {
+}
+
 sub connect {
     my $class    = shift;
     my $dsn      = shift;

lib/SQL/DBx/Sequence.pm
@@ -7,29 +7,19 @@ use Carp qw/croak carp confess/;
 
 our $VERSION = '0.97_3';
 
-has '_sqlite_seq_dbh' => ( is => 'ro' );
+after BUILD => sub {
+    my $self = shift;
+    return unless $self->dbd eq 'SQLite';
 
-around BUILDARGS => sub {
-    my $orig  = shift;
-    my $class = shift;
-    my %args  = @_;
+    my $rows = $self->conn->dbh->selectall_arrayref('PRAGMA database_list');
 
-    $args{dsn} || confess 'Missing argument: dsn';
-    my ( $dbi, $dbd, @rest ) = DBI->parse_dsn( $args{dsn} );
-
-    if ( $dbd eq 'SQLite' and $args{dsn} !~ m/\.seq$/ ) {
-        my $dsn = $args{dsn} . '.seq';
-        my $dbh = DBI->connect(
-            $dsn, '', '',
-            {
-                RaiseError => 1,
-                PrintError => 0,
-            }
-        );
-        $args{_sqlite_seq_dbh} = $dbh;
+    foreach my $row (@$rows) {
+        if ( $row->[1] eq 'main' ) {
+            $self->conn->dbh->do("ATTACH '$row->[2].seq' AS seq");
+            last;
+        }
     }
 
-    return $class->$orig(%args);
 };
 
 sub create_sequence {
@@ -37,9 +27,9 @@ sub create_sequence {
     my $name = shift;
 
     if ( $self->dbd eq 'SQLite' ) {
-        $self->_sqlite_seq_dbh->do( 'CREATE TABLE sequence_' 
+        $self->conn->dbh->do( 'CREATE TABLE seq.' 
               . $name . ' ('
-              . 'seq INTEGER PRIMARY KEY, mtime TIMESTAMP )' );
+              . 'seq INTEGER PRIMARY KEY, x INTEGER )' );
     }
     elsif ( $self->dbd eq 'Pg' ) {
         $self->conn->run(
@@ -58,15 +48,11 @@ sub nextval {
     my $name = shift;
 
     if ( $self->dbd eq 'SQLite' ) {
-        $log->debug( 'INSERT INTO sequence_' 
-              . $name
-              . "(mtime) VALUES(CURRENT_TIMESTAMP)" );
+        $log->debug( 'INSERT INTO seq.' . $name . "(x) VALUES(NULL)" );
 
-        $self->_sqlite_seq_dbh->do( 'INSERT INTO sequence_' 
-              . $name
-              . '(mtime) VALUES(CURRENT_TIMESTAMP)' );
+        $self->conn->dbh->do( 'INSERT INTO seq.' . $name . '(x) VALUES(NULL)' );
 
-        return $self->_sqlite_seq_dbh->sqlite_last_insert_rowid();
+        return $self->conn->dbh->sqlite_last_insert_rowid();
     }
     elsif ( $self->dbd eq 'Pg' ) {
         $log->debug( "SELECT nextval('seq_" . $name . "')" );

commit 46912e53f9f007cca47c82cc04d73e8b99c43b8d

Create test for sequence rollback behaviour.
This is to keep track of SQLite which currently fails, and was
probably introduced as a problem by commit 49404cf9a. Don't know yet
what impact this has and whether it is worth fixing.

t/12-sequence.t
@@ -48,8 +48,37 @@ foreach my $handle (@handles) {
     eval { $db->conn->dbh->do('DROP SEQUENCE seq_testseq'); };
 
     $db->create_sequence('testseq');
-    my $id = $db->nextval('testseq');
-    ok $id, 'nextval';
+
+    my ( $id1, $id2, $id3, $id4 );
+
+    $id1 = $db->nextval('testseq');
+    ok $id1, 'nextval:' . $id1;
+
+    $id2 = $db->nextval('testseq');
+    ok $id2 > $id1, "$id2 > $id1";
+
+    eval {
+        $db->txn(
+            sub {
+                $id3 = $db->nextval('testseq');
+                die;    # Force a ROLLBACK
+            }
+        );
+    };
+
+  TODO: {
+        local $TODO = "SQLite Sequence Rollback"
+          if ( $handle->dbd eq 'SQLite' );
+
+        $id4 = $db->nextval('testseq');
+        ok $id4 > $id3, "$id4 > $id3 after ROLLBACK";
+    }
+
+    if ( $handle->dbd eq 'SQLite' ) {
+        my $rows =
+          $db->conn->dbh->selectrow_array('SELECT count(seq) from seq.testseq');
+        is $rows, 1, 'SQLite rows deleted';
+    }
 }
 
 done_testing();