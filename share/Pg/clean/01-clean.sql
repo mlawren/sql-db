47c821f6d550f924114a4ca436625a79a623e0d3
Mark Lawrence <nomad@null.net>
2012-07-16 20:20:48 +1200

    Clean: drop sequences before tables on PostgreSQL
    Some sequences are automatically dropped when tables are dropped, and
    DROP SEQUENCE commands generated before the tables were dropped would fail.

--- share/Pg/clean/01-clean.sql
+++ share/Pg/clean/01-clean.sql
@@ -8,22 +8,6 @@ BEGIN
     cmd := '';
 
     FOR rec IN SELECT
-            'DROP TABLE ' || quote_ident(n.nspname) || '.'
-                || quote_ident(c.relname) || ' CASCADE;' AS name
-        FROM
-            pg_catalog.pg_class AS c
-        LEFT JOIN
-            pg_catalog.pg_namespace AS n
-        ON
-            n.oid = c.relnamespace WHERE relkind = 'r' AND
-            n.nspname NOT IN ('pg_catalog', 'pg_toast') AND
-            pg_catalog.pg_table_is_visible(c.oid)
-    LOOP
-        cmd := cmd || rec.name;
-    END LOOP;
-
-
-    FOR rec IN SELECT
             'DROP SEQUENCE ' || quote_ident(n.nspname) || '.'
                 || quote_ident(c.relname) || ' CASCADE;' AS name
         FROM
@@ -34,6 +18,21 @@ BEGIN
             n.oid = c.relnamespace
         WHERE
             relkind = 'S' AND
+            n.nspname NOT IN ('pg_catalog', 'pg_toast') AND
+            pg_catalog.pg_table_is_visible(c.oid)
+    LOOP
+        cmd := cmd || rec.name;
+    END LOOP;
+
+    FOR rec IN SELECT
+            'DROP TABLE ' || quote_ident(n.nspname) || '.'
+                || quote_ident(c.relname) || ' CASCADE;' AS name
+        FROM
+            pg_catalog.pg_class AS c
+        LEFT JOIN
+            pg_catalog.pg_namespace AS n
+        ON
+            n.oid = c.relnamespace WHERE relkind = 'r' AND
             n.nspname NOT IN ('pg_catalog', 'pg_toast') AND
             pg_catalog.pg_table_is_visible(c.oid)
     LOOP
