a05f549d2414f93eaa895cec70ea6284cc0fb776
Mark Lawrence <nomad@null.net>
2012-08-20 11:49:25 +1200

    Raise exception when attempting to deploy into the past

--- lib/SQL/DBx/Deploy.pm
+++ lib/SQL/DBx/Deploy.pm
@@ -212,7 +212,10 @@ sub _deploy {
     }
 
     my $latest_change_id = $self->last_deploy_id($app);
-    $log->debug( 'Latest Change ID:', $latest_change_id );
+    $log->debug( 'Current Change ID:',   $latest_change_id );
+    $log->debug( 'Requested Change ID:', scalar @$ref );
+
+    die "Requested Change ID is in the past!" if @$ref < $latest_change_id;
 
     my $count = 0;
     foreach my $cmd (@$ref) {
